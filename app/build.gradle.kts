8d1e1a8de6af2e6056dbb50f215b8b9c62df94ca
HemantPraTechSaga <hemantpra.techsaga@gmail.com>
2025-11-14 16:19:36 +0530

    hemant-patch-commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.mvvmdemo"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
